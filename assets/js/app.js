9b0a05df17cab945d2d15332bd2049c34b01879e
emilyfrels <emily.n.frels@gmail.com>
2020-10-05 07:01:58 -0500

    pulling in data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
 // @TODO: YOUR CODE HERE!
+// console.log because we can
+console.log("This is app.js");
+
+// pull in data from data.csv
+d3.csv("./assets/data/data.csv").then(function(stateData) {
+
+    // confirm we are pulling in data correctly
+    console.log(stateData);
+
+});
